42d08e2a920084380b38202bdbf88beaaac5d06f
JPNYKW <ilikegamesme@gmail.com>
2019-08-20 20:22:17 +0900

    Add header for CORS

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uuidv4 = require('uuid/v4');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
 app.get('/api/v1/check-text', (request, response) => {
 	if (request.body === undefined) {
 		response.json(false);
 	}
 
-	const text = request.body === undefined ? 'undefined' : request.body.text;
+	const text = request.body.text;
 
 	// const uuid = uuidv4();
 	// console.log('\npost:', text, '\nuuid:', uuid);
