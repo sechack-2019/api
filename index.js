a58adba24e2ac189f61230ba899bdf8f51f7d521
JPNYKW <ilikegamesme@gmail.com>
2019-08-20 18:39:10 +0900

    Fix

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const uuidv4 = require('uuid/v4');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get('/api/v1/check-text', (req, res) => {
-	const text = req.body === undefined ? 'undefined' : req.body.text;
-	const uuid = uuidv4();
+app.get('/api/v1/check-text', (request, response) => {
+	if (request.body === undefined) {
+		response.json(false);
+	}
 
+	const text = request.body === undefined ? 'undefined' : request.body.text;
+
+	// const uuid = uuidv4();
 	// console.log('\npost:', text, '\nuuid:', uuid);
 	// console.log('text', text);
 	
+	/*
 	const flag = false;
 
 	const data = [
 		{ uuid, text },
 		{ uuid, flag }
 	];
+	*/
 
-	res.json(data);
+	response.json(true);
 });
 
 const port = process.env.PORT || 5000;
