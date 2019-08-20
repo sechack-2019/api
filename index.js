6eda5404be8d41a9e7779d1436b0a1b64f6754f0
JPNYKW <ilikegamesme@gmail.com>
2019-08-20 23:09:12 +0900

    Update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uuidv4 = require('uuid/v4');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
@@ -22,10 +24,27 @@ app.post('/api/v1/post', (request, response) => {
     // const data = request.body;
     // const text = data.text;
 
-	console.log(request.body.text || 'No Text');
+	const text = request.body.text || 'No Text';
+	console.log('Text:', text);
 
-    response.json({text: 'hello'});
+    response.json({text});
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// usage
+/*
+
+fetch('https://salutem-api.herokuapp.com/api/v1/post', {
+	method: 'POST',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		text: 'hoge'
+	})
+}).then(res => res.json()).then(res => console.log(res))
+
+*/
